ff36c989e6901e18d187a676b179e37d68246216
ele_11_ven <lwentao@hotmail.com>
2022-08-11 06:14:19 +0000

    cached

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 # from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 class Base(View):
-    def index(self):
-        
-        return HttpResponse("Hello async world!")
+
+    @require_http_methods(['GET'])
+    def index(request):
+        # print(request.GET.get('id', default=None))
+        return JsonResponse({ "name": "张三", "id": request.GET.get('id', default=None) })
