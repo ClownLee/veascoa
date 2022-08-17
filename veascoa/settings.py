2867e466a1a7b14445aa7ecf0b17c566c7189b22
ele_11_ven <lwentao@hotmail.com>
2022-08-17 01:42:45 +0000

    gitignore

--- veascoa/settings.py
+++ veascoa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*^kr8&1e9b_7%it^+dz58@wp%lpl#c8&)%6(80i8s_y=(p*m+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
