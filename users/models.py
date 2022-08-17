acd19a1ec1026b2ecc128b849c7d2806971c90ba
ele_11_ven <lwentao@hotmail.com>
2022-08-17 04:08:34 +0000

    add salt

--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Users(models.Model):
     avator = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     salt = models.CharField(max_length=255)
-    create_time = models.DateTimeField()
-    update_time = models.DateTimeField()
+    create_time = models.DateTimeField(null = True)
+    update_time = models.DateTimeField(null = True)
     class Meta:
         db_table = 'users'
         managed = False
