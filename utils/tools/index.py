748332fc233f7a74f4868651b1f0e68f9342e6ca
ele_11_ven <lwentao@hotmail.com>
2022-08-18 09:24:45 +0000

    jwt

--- utils/tools/index.py
+++ utils/tools/index.py
@@ -18,10 +18,10 @@ class Tools:
         encoded_jwt = jwt.encode({
             "exp": datetime.datetime.now() + datetime.timedelta(seconds=172800), # 两天后过期
             "user": dict(userinfo),
-        }, SIGN_KEY, algorithms=["HS256"])
+        }, SIGN_KEY, ["HS256"])
 
         return encoded_jwt
 
     def verifySign(token):
-        decoded = jwt.decode(token, SIGN_KEY, algorithms=["HS256"])
+        decoded = jwt.decode(token, SIGN_KEY, ["HS256"])
         print(decoded)
