e859a66422e869a6602bc1951d2324e644a819de
ele_11_ven <lwentao@hotmail.com>
2022-08-18 09:23:16 +0000

    去掉中间件拦截

--- middleware/interceptor.py
+++ middleware/interceptor.py
@@ -3,8 +3,8 @@ from django.utils.deprecation import MiddlewareMixin
 class Interceptor(MiddlewareMixin):
     def process_request(self, request):
         print('在请求到达路由之前调用我')
-        if request.META.get('HTTP_AUTHORIZATION') != "asdaisofkjwert0witwoerfpwoer-wq0er-0ioifs;fp'sdpfasfalsfp566fasfd":
-        	raise Exception('签名错误')
+        """ if request.META.get('HTTP_AUTHORIZATION') != "asdaisofkjwert0witwoerfpwoer-wq0er-0ioifs;fp'sdpfasfalsfp566fasfd":
+        	raise Exception('签名错误') """
         
     def process_view(self, request, callback, callback_args, callback_kwargs):
         print('调用视图之前先调用我')
